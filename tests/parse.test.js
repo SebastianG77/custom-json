902270466e1540fa4993661ff2730c3efc921541
SebastianG <s.gostomski@hotmail.de>
2021-02-15 21:39:13 +0000

    test(parse): add first test case for testing parse function

---
+++ tests/parse.test.js
@@ -0,0 +1,13 @@
+const customJSON = require('../src/parse')
+
+const { describe, expect, it } = global
+
+// const ROOT_DIRECTORY = path.join(__dirname, '..', '..')
+
+describe('Validate a JSON file that does not contain any duplicates', () => {
+  it('returns an empty list', () => {
+    const jsonString = '{"mykey": "myvalue"}'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return 'Hello World!' } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: 'Hello World!' })
+  })
+})
