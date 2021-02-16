49a1dc090ff6755ca1ceff5c3ed0f86cc43c4eee
SebastianG <s.gostomski@hotmail.de>
2021-02-16 18:52:56 +0000

    test(parse): add test cases for testing simple boolean and numeric replacements

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -2,10 +2,34 @@ const customJSON = require('../src/parse')
 
 const { describe, expect, it } = global
 
-describe('Replace string key', () => {
+describe('Replace string value', () => {
   it('returns the expected JSON object', () => {
     const jsonString = '{"mykey": "myvalue"}'
     const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return 'Hello World!' } else { return originalValue } })
     expect(parsedJSON).toEqual({ mykey: 'Hello World!' })
+  })
+})
+
+describe('Replace false boolean value', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": false}'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'boolean') { return true } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: true })
+  })
+})
+
+describe('Replace true boolean value', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": false}'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'boolean') { return true } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: true })
+  })
+})
+
+describe('Replace numeric value', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": 100}'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'number') { return 222 } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: 222 })
   })
 })
