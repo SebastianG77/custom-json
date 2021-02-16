85d7ae73a541166949180760db67fb417e600daa
SebastianG <s.gostomski@hotmail.de>
2021-02-16 17:39:31 +0000

    docs(parse): fix description text of test case

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -2,8 +2,8 @@ const customJSON = require('../src/parse')
 
 const { describe, expect, it } = global
 
-describe('Validate a JSON file that does not contain any duplicates', () => {
-  it('returns an empty list', () => {
+describe('Replace string key', () => {
+  it('returns the expected JSON object', () => {
     const jsonString = '{"mykey": "myvalue"}'
     const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return 'Hello World!' } else { return originalValue } })
     expect(parsedJSON).toEqual({ mykey: 'Hello World!' })
