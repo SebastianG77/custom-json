4235ffe73742f077e61c7d7ae5d04ff132797092
SebastianG <s.gostomski@hotmail.de>
2021-02-17 17:57:35 +0000

    test(parse): add further test cases resulting in 100% code coverage

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -34,3 +34,86 @@ describe('Replace numeric value', () => {
     expect(parsedJSON).toEqual({ mykey: 222 })
   })
 })
+
+describe('Replace negative numeric value by positive value', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": -100}'
+    const newValue = 2
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'number') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: newValue })
+  })
+})
+
+describe('Replace negative numeric value by negative value', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": -1}'
+    const newValue = -22
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'number') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: newValue })
+  })
+})
+
+describe('Replace string value by null', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": "myvalue"}'
+    const newValue = null
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: newValue })
+  })
+})
+
+describe('Replace string value by null', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": "myvalue"}'
+    const newValue = null
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: newValue })
+  })
+})
+
+describe('Replace null value by String', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": null}'
+    const newValue = 'myvalue'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (originalValue === null) { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: newValue })
+  })
+})
+
+describe('Replace array of string values', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": ["first", "second"]}'
+    const newValue = 'newValue'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: [newValue, newValue] })
+  })
+})
+
+describe('Replace an object by a new object', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": "myvalue"}'
+    const newValue = { newKey: 'newValue' }
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'object') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual(newValue)
+  })
+})
+
+describe('Replace an object and its contained value', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": "myvalue"}'
+    const newStringValue = 'myvalue'
+    const newObjectValue = { newKey: 'newValue' }
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'object') { return newObjectValue } else if (typeof originalValue === 'string') { return newStringValue } else { return originalValue } })
+    console.log('parsedjson ' + JSON.stringify(parsedJSON, null, 2))
+    expect(parsedJSON).toEqual(newObjectValue)
+  })
+})
+
+describe('Replace string value with escaped quotationsmarks', () => {
+  it('returns the expected JSON object', () => {
+    const jsonString = '{"mykey": "my\\"value\\""}'
+    const newValue = 'newValue'
+    const parsedJSON = customJSON.parse(jsonString, (key, originalValue, stringValue, jsonObject, parentKeys) => { if (typeof originalValue === 'string') { return newValue } else { return originalValue } })
+    expect(parsedJSON).toEqual({ mykey: newValue })
+  })
+})
